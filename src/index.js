c189b3552e78969c3e98ebde7f8fce7c16093cd1
Vishal Mistry <vishalmistry40@gmail.com>
2019-09-21 21:28:49 -0700

    Added Logger Midleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
 import reduxthunk from "redux-thunk";
 import "./index.css";
 import App from "./App";
@@ -13,11 +14,12 @@ const initialState = {
   recommendations: []
 };
 
+const logger = createLogger();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   appReducer,
   initialState,
-  composeEnhancers(applyMiddleware(reduxthunk))
+  composeEnhancers(applyMiddleware(reduxthunk,logger))
 );
 
 ReactDOM.render(
